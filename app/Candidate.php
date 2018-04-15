f3588866f0bcf50a2c1de88925eb9f4e2f3766ff
Alexander Viertel <alexander-viertel@web.de>
2018-04-15 16:03:08 +0200

    added PHPDoc Comment for ide completion

--- app/Candidate.php
+++ app/Candidate.php
@@ -8,8 +8,28 @@
 
 namespace App;
 
-
 use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Candidate
+ * @package App\Models
+ *
+ * @property int      $id
+ * @property int|null $service_partner_id
+ * @property string   $name
+ *
+ *
+ * @method static Candidate create($value)
+ * @method static Candidate find($value)
+ * @method static Candidate findOrFail($value)
+ * @method static Candidate whereCreatedAt($value)
+ * @method static Candidate whereId($value)
+ * @method static Candidate whereName($value)
+ * @method static Candidate whereServicePartnerId($value)
+ * @method static Candidate whereUpdatedAt($value)
+ * @method static Candidate whereHas($relation, \Closure $callback = null, $operator = '>=', $count = 1)
+ * @method static Candidate where($column, $operator = null, $value = null, $boolean = 'and')
+ */
 
 class Candidate extends Model
 {
